fe49a11206a1c1685ad26026b2af7c438b41ce53
Akil Darjean <akil.darjean@searchspring.com>
2020-09-01 11:40:11 -0600

    Improve tests for 'getCoffeeDates()'

--- index.test.ts
+++ index.test.ts
@@ -1,8 +1,11 @@
 const { getCoffeeDates, splitUsersIntoPairs } = require('./index');
 
 test('getCoffeeDates() - returns an array of coffee date pairs when an array of user IDs is inputted', () => {
-    expect(getCoffeeDates(['U023BECGF', 'U061F7AUR', 'U023EJJWO', 'U061A3ZUR'])[0].users.length).toBe(2);
-    expect(getCoffeeDates(['U023BECGF', 'U061F7AUR', 'U023EJJWO', 'U061A3ZUR'])[1].users.length).toBe(2);
+    const dates = getCoffeeDates(['U023BECGF', 'U061F7AUR', 'U023EJJWO', 'U061A3ZUR']);
+
+    expect(dates.length).toBe(2);
+    expect(dates[0].users.length).toBe(2);
+    expect(dates[1].users.length).toBe(2);
 });
 
 test('splitUsersIntoPairs() - splits array into an array of randomized pairs of two elements', () => {
