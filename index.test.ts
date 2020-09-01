3a2578b20d895a64b3297f63a96c955f7dd67c96
Akil Darjean <akil.darjean@searchspring.com>
2020-09-01 11:41:46 -0600

    Improve tests for 'splitUsersIntoPairs()'

--- index.test.ts
+++ index.test.ts
@@ -9,5 +9,10 @@ test('getCoffeeDates() - returns an array of coffee date pairs when an array of 
 });
 
 test('splitUsersIntoPairs() - splits array into an array of randomized pairs of two elements', () => {
-    expect(splitUsersIntoPairs(['1', '2', '3', '4', '5', '6'])[0].length).toBe(2);
+    const pairs = splitUsersIntoPairs(['1', '2', '3', '4', '5', '6']);
+
+    expect(pairs.length).toBe(3);
+    expect(pairs[0].length).toBe(2);
+    expect(pairs[1].length).toBe(2);
+    expect(pairs[2].length).toBe(2);
 });
