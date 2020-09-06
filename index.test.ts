150404c1d03bff54df4e3184110004f9c5c92757
Akil Darjean <akil.darjean@searchspring.com>
2020-09-06 16:51:17 -0600

    Reorder tests

--- index.test.ts
+++ index.test.ts
@@ -1,13 +1,5 @@
 const { getCoffeeDates, splitUsersIntoPairs } = require('./index');
 
-test('getCoffeeDates() - returns an array of coffee date pairs when an array of user IDs is inputted', () => {
-    const dates = getCoffeeDates(['U023BECGF', 'U061F7AUR', 'U023EJJWO', 'U061A3ZUR']);
-
-    expect(dates.length).toBe(2);
-    expect(dates[0].users.length).toBe(2);
-    expect(dates[1].users.length).toBe(2);
-});
-
 test('splitUsersIntoPairs() - splits array into an array of randomized pairs of two elements', () => {
     const pairs = splitUsersIntoPairs(['1', '2', '3', '4', '5', '6']);
 
@@ -24,4 +16,12 @@ test('splitUsersIntoPairs() - array with odd number of elements creates a group 
     expect(pairs[0].length).toBe(2);
     expect(pairs[1].length).toBe(2);
     expect(pairs[2].length).toBe(3);
+});
+
+test('getCoffeeDates() - returns an array of coffee date pairs when an array of user IDs is inputted', () => {
+    const dates = getCoffeeDates(['U023BECGF', 'U061F7AUR', 'U023EJJWO', 'U061A3ZUR']);
+
+    expect(dates.length).toBe(2);
+    expect(dates[0].users.length).toBe(2);
+    expect(dates[1].users.length).toBe(2);
 });
