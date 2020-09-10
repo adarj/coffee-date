2fae7df99aa54bcdb47b1d84616960aed654dd2f
Akil Darjean <akil.darjean@searchspring.com>
2020-09-10 14:51:31 -0600

    Convert tests into describe-it format

--- index.test.ts
+++ index.test.ts
@@ -1,27 +1,31 @@
 const { getCoffeeDates, splitUsersIntoPairs } = require('./index');
 
-test('splitUsersIntoPairs() - splits array into an array of randomized pairs of two elements', () => {
-    const pairs = splitUsersIntoPairs(['1', '2', '3', '4', '5', '6']);
+describe('splitUsersIntoPairs()', () => {
+    it('splits array into an array of randomized pairs of two elements', () => {
+        const pairs = splitUsersIntoPairs(['1', '2', '3', '4', '5', '6']);
 
-    expect(pairs.length).toBe(3);
-    expect(pairs[0].length).toBe(2);
-    expect(pairs[1].length).toBe(2);
-    expect(pairs[2].length).toBe(2);
+        expect(pairs.length).toBe(3);
+        expect(pairs[0].length).toBe(2);
+        expect(pairs[1].length).toBe(2);
+        expect(pairs[2].length).toBe(2);
+    });
+
+    it('array with odd number of elements creates a group with three members', () => {
+        const pairs = splitUsersIntoPairs(['1', '2', '3', '4', '5', '6', '7']);
+
+        expect(pairs.length).toBe(3);
+        expect(pairs[0].length).toBe(2);
+        expect(pairs[1].length).toBe(2);
+        expect(pairs[2].length).toBe(3);
+    });
 });
 
-test('splitUsersIntoPairs() - array with odd number of elements creates a group with three members', () => {
-    const pairs = splitUsersIntoPairs(['1', '2', '3', '4', '5', '6', '7']);
-
-    expect(pairs.length).toBe(3);
-    expect(pairs[0].length).toBe(2);
-    expect(pairs[1].length).toBe(2);
-    expect(pairs[2].length).toBe(3);
-});
-
-test('getCoffeeDates() - returns an array of coffee date pairs when an array of user IDs is inputted', () => {
-    const dates = getCoffeeDates(['U023BECGF', 'U061F7AUR', 'U023EJJWO', 'U061A3ZUR']);
-
-    expect(dates.length).toBe(2);
-    expect(dates[0].users.length).toBe(2);
-    expect(dates[1].users.length).toBe(2);
+describe('getCoffeeDates()', () => {
+    it('returns an array of coffee date pairs when an array of user IDs is inputted', () => {
+        const dates = getCoffeeDates(['U023BECGF', 'U061F7AUR', 'U023EJJWO', 'U061A3ZUR']);
+    
+        expect(dates.length).toBe(2);
+        expect(dates[0].users.length).toBe(2);
+        expect(dates[1].users.length).toBe(2);
+    });
 });
