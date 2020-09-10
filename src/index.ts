d272f2efda0f9fd11677fb54b2707d7d5a7701c4
Akil Darjean <akil.darjean@searchspring.com>
2020-09-10 16:39:11 -0600

    Add events listener

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 const { WebClient } = require('@slack/web-api');
+const { createEventAdapter } = require('@slack/events-api');
 const shuffle = require('shuffle-array');
 
 const web = new WebClient(process.env.SLACK_TOKEN);
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 
 interface CoffeeDate {
     users: string[];
@@ -47,5 +49,13 @@ function postMessage(channel: string, text: string): void {
         console.log('Message posted!');
     })();
 }
+
+const port = process.env.port || 3000;
+
+(async () => {
+    const server = await slackEvents.start(port);
+
+    console.log(`Listening for events on ${server.address().port}`);
+})();
 
 module.exports = { getCoffeeDates, splitUsersIntoPairs };
